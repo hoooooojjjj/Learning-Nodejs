73158c1ca6d1e1a298f23cc938c74b9e267d28ae
sono <rhj080471@gmail.com>
2024-04-23 23:03:08 +0900

    Feat : 서버에서 라우팅하기

--- basic/server1.js
+++ basic/server1.js
@@ -2,9 +2,21 @@ const http = require("http");
 
 // 서버 만들기
 const server = http.createServer((req, res) => {
+  // req.url : 요청 경로
+  // req.method : 요청 방식
   res.setHeader("Content-type", "text/plain");
-  res.write("hello node");
-  res.end();
+  const { url, method } = req;
+  if (method === "GET" && url === "/home") {
+    res.write("HOME");
+    res.end();
+  } else if (method === "GET" && url === "/about") {
+    res.end("ABOUT");
+  } else {
+    res.end("NOT FOUND");
+  }
+
+  // res.write("hello node");
+  // res.end();
 });
 
 // 서버 실행
