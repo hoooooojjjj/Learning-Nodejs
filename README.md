de21b71360c7a33f9f9de6989fbec05d32226007
sono <rhj080471@gmail.com>
2024-04-26 21:03:50 +0900

    Feat : 익스프레스로 라우팅 - 라우트 파라미터 사용 (Thunder client)

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
   - 내장 모듈1(노드 코어 모듈) : path, fs, http 등
   - 내장 모듈2(글로벌 모듈 - require 없이 사용 가능) : console, '\_\_dirname', '\_\_filename' 등
   - npm 모듈
+- express 프레임워크
+  - 익스프레스를 통해 서버 만들고 실행
+  - 익스프레스 라우팅(get,post,put,delete)
+    - Thunder Client(Postman 대체) : 가상 클라이언트 요청
+    - 라우트 파라미터
--- myContacts/app.js
+++ myContacts/app.js
@@ -3,12 +3,34 @@ const express = require("express");
 // app이라는 이름을 가진 서버 만들기
 const app = express();
 
+// app 서버를 통해 서버 응답
 app.get("/", (req, res) => {
   res.send("hello node");
 });
 
+// 연락처 전체 목록 가져오기
 app.get("/contacts", (req, res) => {
   res.send("Contacts Page");
+});
+
+// 새로운 연락처 추가
+app.post("/contacts", (req, res) => {
+  res.send("Create Contacts");
+});
+
+// 특정 연락처 가져오기(라우트 파라미터)
+app.get("/contacts/:id", (req, res) => {
+  res.send(`View Contact for ID : ${req.params.id}`);
+});
+
+// 특정 연락처 수정하기
+app.put("/contacts/:id", (req, res) => {
+  res.send(`Update Contact for ID : ${req.params.id}`);
+});
+
+// 특정 연락처 삭제하기
+app.delete("/contacts/:id", (req, res) => {
+  res.send(`Delete Contact for ID : ${req.params.id}`);
 });
 
 // 서버 실행
